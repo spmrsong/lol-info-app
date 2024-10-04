93cc0505ec84305ea9c0f182d6985577ef9b9ca4
James <pok_beg@naver.com>
2024-10-04 10:58:22 +0900

    fix: change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ddragon.leagueoflegends.com", "cmsassets.rgpub.io"],
+    domains: [
+      "ddragon.leagueoflegends.com",
+      "cmsassets.rgpub.io",
+      "https://lol-info-app-1st.vercel.app",
+    ],
   },
 };
 
